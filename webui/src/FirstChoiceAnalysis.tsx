f60eda065fba8dd34e569604889b7e52371de796
akazukin5151 <tsuiyikching@protonmail.com>
2025-07-27 16:45:52 +0800

    feat: add sankey

--- webui/src/FirstChoiceAnalysis.tsx
+++ webui/src/FirstChoiceAnalysis.tsx
@@ -10,6 +10,7 @@ import {
   type ChartData,
 } from "chart.js";
 import { Chart } from "react-chartjs-2";
+import { SankeyController, Flow } from "chartjs-chart-sankey";
 import type { Setter } from "./core";
 
 ChartJS.register(
@@ -19,7 +20,27 @@ ChartJS.register(
   LinearScale,
   BarElement,
   Colors,
+  SankeyController,
+  Flow,
 );
+
+const CHART_COLORS = Object.values({
+  red: "rgb(255, 99, 132)",
+  orange: "rgb(255, 159, 64)",
+  yellow: "rgb(255, 205, 86)",
+  green: "rgb(75, 192, 192)",
+  blue: "rgb(54, 162, 235)",
+  purple: "rgb(153, 102, 255)",
+  grey: "rgb(201, 203, 207)",
+});
+
+type SankeyData = {
+  from: string;
+  to: string;
+  flow: number;
+  fromIdx: number;
+  toIdx: number;
+};
 
 type FirstChoiceAnalysisProps = {
   cands: Array<string>;
@@ -45,7 +66,29 @@ export function FirstChoiceAnalysis({
     datasets: [],
   };
 
-  if (laterChoices.length > 0) {
+  const sankeyChartData: ChartData<"sankey", Array<SankeyData>, string> = {
+    datasets: [
+      {
+        data: [],
+        colorFrom: (c) => {
+          const sd = c.dataset.data[c.dataIndex] as SankeyData;
+          return CHART_COLORS[sd.fromIdx - 1];
+        },
+        colorTo: (c) => {
+          const sd = c.dataset.data[c.dataIndex] as SankeyData;
+          return CHART_COLORS[sd.toIdx - 1];
+        },
+        // TODO: not working
+        // column: {
+        //   "2. Exhausted": 1,
+        //   "3. Exhausted": 2,
+        //   "4. Exhausted": 3,
+        // },
+      },
+    ],
+  };
+
+  if (laterChoices.length > 0 && firstChoiceCand != null) {
     chartData.labels = [
       ...cands.filter((cand) => cand !== firstChoiceCand),
       "Exhausted",
@@ -87,11 +130,54 @@ export function FirstChoiceAnalysis({
       const dataset = {
         label: `${choice_num}${str} choice`,
         data,
+        backgroundColor: CHART_COLORS[choice_idx + 1],
+        borderColor: CHART_COLORS[choice_idx + 1],
       };
       datasets.push(dataset);
     }
 
     chartData.datasets = datasets;
+
+    const flows: Map<string, Map<string, number>> = new Map();
+    laterChoices.forEach((ballot) => {
+      for (let i = 0; i < ballot.length + 1; i++) {
+        if (i === ballot.length && i + 2 === 6) {
+          continue;
+        }
+
+        const prev_choice = i === 0 ? firstChoiceCand : ballot[i - 1];
+        const this_choice = i === ballot.length ? "Exhausted" : ballot[i];
+
+        const from = `${i + 1}: ${prev_choice}`;
+        const to = `${i + 2}: ${this_choice}`;
+
+        const inner_map = flows.get(from);
+        if (inner_map != null) {
+          const flow = inner_map.get(to);
+          if (flow != null) {
+            inner_map.set(to, flow + 1);
+          } else {
+            inner_map.set(to, 1);
+          }
+        } else {
+          const inner_map = new Map();
+          inner_map.set(to, 1);
+          flows.set(from, inner_map);
+        }
+      }
+    });
+
+    for (const [from, inner_map] of flows.entries()) {
+      for (const [to, flow] of inner_map.entries()) {
+        sankeyChartData.datasets[0].data.push({
+          from,
+          to,
+          flow,
+          fromIdx: parseInt(from.slice(0, 1)),
+          toIdx: parseInt(to.slice(0, 1)),
+        });
+      }
+    }
   }
 
   const handleCandidateSelect = async (cand: string) => {
@@ -151,7 +237,7 @@ export function FirstChoiceAnalysis({
         <p>first, their later choices were:</p>
       </div>
 
-      <div style={{ height: "80%" }}>
+      <div style={{ height: "80%" }} className="mb-6">
         {isComputing ? (
           <div className="h-full w-full rounded-xl bg-neutral-100"></div>
         ) : (chartData.labels?.length ?? 0) > 0 ? (
@@ -174,6 +260,33 @@ export function FirstChoiceAnalysis({
           />
         ) : (
           <p>Please select a candidate first</p>
+        )}
+      </div>
+
+      <div className="h-full pb-3">
+        {isComputing ? (
+          <div className="h-full w-full rounded-xl bg-neutral-100"></div>
+        ) : (
+          (sankeyChartData.datasets[0].data.length ?? 0) > 0 && (
+            <Chart
+              type="sankey"
+              data={sankeyChartData}
+              options={{
+                animation: false,
+                responsive: true,
+                maintainAspectRatio: false,
+                indexAxis: "y",
+                scales: {
+                  x: {
+                    stacked: true,
+                  },
+                  y: {
+                    stacked: true,
+                  },
+                },
+              }}
+            />
+          )
         )}
       </div>
     </>
