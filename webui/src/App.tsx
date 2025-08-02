a7c57fa34273fbda71e543529aa9849197fd4013
akazukin5151 <tsuiyikching@protonmail.com>
2025-08-02 18:07:42 +0800

    refactor: fix lints

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -25,6 +25,29 @@ function App() {
           const cands = cands_csv.split("\t").filter((cand) => cand !== "");
           setCands(cands);
           handleCandidateSelectCore(0, setLaterChoices, setFlowData);
+
+          fetch("rank-distributions.tsv")
+            .then((x) => x.text())
+            .then((tsv) => {
+              const rank_distributions_csv = tsv.split("\n");
+              const rank_dist_data: Array<Array<number>> = [];
+              for (const row of rank_distributions_csv.slice(1)) {
+                if (row.length === 0) {
+                  continue;
+                }
+
+                const splitted = row.split("\t");
+                const cand_idx = parseInt(splitted[0]);
+                const rank = parseInt(splitted[1]) - 1;
+                const freq = parseInt(splitted[2]);
+
+                const cand_arr = rank_dist_data[rank] ?? cands.map(() => 0);
+                cand_arr[cand_idx] = freq;
+                rank_dist_data[rank] = cand_arr;
+              }
+
+              setRankDistData(rank_dist_data);
+            });
         });
 
       fetch("n_voters.tsv")
@@ -32,29 +55,6 @@ function App() {
         .then((n_votes_tsv) => {
           const n_votes = n_votes_tsv.split("\t").map((s) => parseInt(s));
           setAllNVotes(n_votes);
-        });
-
-      fetch("rank-distributions.tsv")
-        .then((x) => x.text())
-        .then((tsv) => {
-          const rank_distributions_csv = tsv.split("\n");
-          const rank_dist_data: Array<Array<number>> = [];
-          for (const row of rank_distributions_csv.slice(1)) {
-            if (row.length === 0) {
-              continue;
-            }
-
-            const splitted = row.split("\t");
-            const cand_idx = parseInt(splitted[0]);
-            const rank = parseInt(splitted[1]) - 1;
-            const freq = parseInt(splitted[2]);
-
-            const cand_arr = rank_dist_data[rank] ?? cands.map(() => 0);
-            cand_arr[cand_idx] = freq;
-            rank_dist_data[rank] = cand_arr;
-          }
-
-          setRankDistData(rank_dist_data);
         });
 
       fetch("matrices.json")
