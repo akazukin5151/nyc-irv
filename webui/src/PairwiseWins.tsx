cf3816bbd0982d28544acaa1680b8321068521ba
akazukin5151 <tsuiyikching@protonmail.com>
2025-08-09 12:57:08 +0800

    feat: make only the pairwise table a card

--- webui/src/PairwiseWins.tsx
+++ webui/src/PairwiseWins.tsx
@@ -12,27 +12,11 @@ export function PairwiseWins() {
 
   return (
     <>
-      <section className="rounded-md bg-white pb-3 shadow-md">
+      <section>
         <h2 className="ml-4 pt-2">Number of pairwise wins</h2>
 
-        <div className="mx-auto flex items-center justify-center pb-3 max-lg:mx-4 max-md:flex-wrap md:justify-between lg:w-[70%]">
-          <ul className="w-sm list-disc [&>li]:my-2">
-            <li>There were 11 candidates for Mayor.</li>
-            <li>Each candidate can have 10 other 1 v 1 matchups.</li>
-            <li>
-              {Mamdani} won all 10 of his matchups, so he is the{" "}
-              <span className="underline decoration-green-600 decoration-3">
-                Condorcet winner
-              </span>
-              .
-            </li>
-            <li>
-              {Mamdani} was also the winner of this IRV (RCV) election, so in
-              this case, IRV successfully elected the Condorcet winner.
-            </li>
-          </ul>
-
-          <table className="max-md:mt-2 [&_td]:border-b-2 [&_td]:border-neutral-200/20 [&_td]:px-3 [&_td]:py-1 [&_td]:nth-[2n]:text-right [&_th]:px-3 [&_th]:py-1 [&_tr]:hover:bg-slate-100/50">
+        <div className="mx-auto flex items-center justify-center py-3 max-lg:mx-4 max-md:flex-wrap md:justify-between lg:w-[70%]">
+          <table className="rounded-xl bg-white pb-3 shadow-md max-md:mt-2 [&_td]:border-b-2 [&_td]:border-neutral-200/20 [&_td]:px-3 [&_td]:py-1 [&_td]:nth-[2n]:text-right [&_th]:px-3 [&_th]:py-1 [&_tr]:hover:bg-slate-100/50">
             <thead
               className="border-b-2 border-slate-200/50 bg-slate-100 text-slate-500"
               style={{ fontVariant: "small-caps" }}
@@ -115,6 +99,22 @@ export function PairwiseWins() {
               </tr>
             </tbody>
           </table>
+
+          <ul className="w-sm list-disc [&>li]:my-2">
+            <li>There were 11 candidates for Mayor.</li>
+            <li>Each candidate can have 10 other 1 v 1 matchups.</li>
+            <li>
+              {Mamdani} won all 10 of his matchups, so he is the{" "}
+              <span className="underline decoration-green-600 decoration-3">
+                Condorcet winner
+              </span>
+              .
+            </li>
+            <li>
+              {Mamdani} was also the winner of this IRV (RCV) election, so in
+              this case, IRV successfully elected the Condorcet winner.
+            </li>
+          </ul>
         </div>
       </section>
     </>
