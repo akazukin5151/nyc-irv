3c6c85d4417a7e08bb54493591c5e2dcf6601bfa
akazukin5151 <tsuiyikching@protonmail.com>
2025-08-09 16:17:38 +0800

    feat: make pairwise matrix scrollable horizontally

--- webui/src/PairwiseMatrix.tsx
+++ webui/src/PairwiseMatrix.tsx
@@ -43,87 +43,91 @@ export function PairwiseMatrix({ cands: cands_ }: PairwiseMatrixProps) {
 
       <PairwiseMatrixHoverInfo hoverInfo={hoverInfo} />
 
-      <table
-        className="mt-3 ml-4 rounded-xl bg-white pb-3 shadow-md [&_td]:px-3 [&_td]:py-1 [&_td]:text-right [&_td]:first:text-left [&_th]:px-3 [&_th]:py-1"
-        onMouseMove={(evt) => {
-          const elem = evt.target as HTMLElement;
-          const value = elem.dataset.value;
-          const this_cand = elem.dataset.row;
-          const other_cand = elem.dataset.col;
-          if (this_cand == null || other_cand == null || matrix == null) {
+      <div className="max-w-screen overflow-auto">
+        <table
+          className="mt-3 ml-4 rounded-xl bg-white pb-3 shadow-md [&_td]:px-3 [&_td]:py-1 [&_td]:text-right [&_td]:first:text-left [&_th]:px-3 [&_th]:py-1"
+          onMouseMove={(evt) => {
+            const elem = evt.target as HTMLElement;
+            const value = elem.dataset.value;
+            const this_cand = elem.dataset.row;
+            const other_cand = elem.dataset.col;
+            if (this_cand == null || other_cand == null || matrix == null) {
+              setHoverInfo(null);
+              return;
+            }
+            const record = matrix.get(other_cand)!;
+
+            setHoverInfo({
+              this_cand,
+              other_cand,
+              votes_for_this: value == null ? null : parseInt(value),
+              votes_for_other: value == null ? null : record[this_cand],
+            });
+          }}
+          onMouseLeave={() => {
             setHoverInfo(null);
-            return;
-          }
-          const record = matrix.get(other_cand)!;
+          }}
+        >
+          <thead className="border-b-2 border-slate-200/50 [&_th]:font-normal">
+            <tr>
+              <th className="border-r-2 border-slate-200/50"></th>
+              {cands.length > 0
+                && cands.map((cand) => (
+                  <th
+                    key={cand}
+                    className={
+                      `text-amber-600 ` + getCellStyle(hoverInfo, "", cand)
+                    }
+                    data-col={cand}
+                  >
+                    {cand}
+                  </th>
+                ))}
+            </tr>
+          </thead>
 
-          setHoverInfo({
-            this_cand,
-            other_cand,
-            votes_for_this: value == null ? null : parseInt(value),
-            votes_for_other: value == null ? null : record[this_cand],
-          });
-        }}
-        onMouseLeave={() => {
-          setHoverInfo(null);
-        }}
-      >
-        <thead className="border-b-2 border-slate-200/50 [&_th]:font-normal">
-          <tr>
-            <th className="border-r-2 border-slate-200/50"></th>
-            {cands.length > 0
-              && cands.map((cand) => (
-                <th
-                  key={cand}
-                  className={
-                    `text-amber-600 ` + getCellStyle(hoverInfo, "", cand)
-                  }
-                  data-col={cand}
-                >
-                  {cand}
-                </th>
-              ))}
-          </tr>
-        </thead>
-
-        <tbody className="[&_tr]:hover:bg-slate-100">
-          {matrix != null
-            && matrix.size > 0
-            && cands.map((this_cand) => {
-              const cols = matrix.get(this_cand);
-              if (cols == null) {
-                return null;
-              }
-              return (
-                <tr key={this_cand}>
-                  <td className="border-r-2 border-slate-200/50 text-sky-500">
-                    {this_cand}
-                  </td>
-                  {cands.map((other_cand) => {
-                    const value = cols[other_cand];
-                    const formatted = isNaN(value)
-                      ? ""
-                      : new Intl.NumberFormat("en-US").format(cols[other_cand]);
-                    return (
-                      <td
-                        key={other_cand}
-                        data-row={this_cand}
-                        data-col={other_cand}
-                        data-value={value}
-                        className={getCellStyle(
-                          hoverInfo,
-                          this_cand,
-                          other_cand,
-                        )}
-                      >
-                        {formatted}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+          <tbody className="[&_tr]:hover:bg-slate-100">
+            {matrix != null
+              && matrix.size > 0
+              && cands.map((this_cand) => {
+                const cols = matrix.get(this_cand);
+                if (cols == null) {
+                  return null;
+                }
+                return (
+                  <tr key={this_cand}>
+                    <td className="border-r-2 border-slate-200/50 text-sky-500">
+                      {this_cand}
+                    </td>
+                    {cands.map((other_cand) => {
+                      const value = cols[other_cand];
+                      const formatted = isNaN(value)
+                        ? ""
+                        : new Intl.NumberFormat("en-US").format(
+                            cols[other_cand],
+                          );
+                      return (
+                        <td
+                          key={other_cand}
+                          data-row={this_cand}
+                          data-col={other_cand}
+                          data-value={value}
+                          className={getCellStyle(
+                            hoverInfo,
+                            this_cand,
+                            other_cand,
+                          )}
+                        >
+                          {formatted}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 }
